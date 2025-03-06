1ac69993dc8411a3c943428b0cd711a113e158b5
dmlar <github@dmlar.com>
2025-03-06 03:46:33 -0800

    fix build/linter errors

--- src/app/rentals/index.tsx
+++ src/app/rentals/index.tsx
@@ -3,7 +3,7 @@ import rentalsData from '@/data/rentals.json';
 export default function RentalsIndex() {
     return <div>{
         rentalsData.map(rental => {
-            return <a href={`rentals/${rental.addressStub}/redirect`}> {rental.address.street} - {rental.title}</a>
+            return <a key={rental.addressStub} href={`rentals/${rental.addressStub}/redirect`}> {rental.address.street} - {rental.title}</a>
         })
     }</div>
 }
