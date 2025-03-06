77a208e82e0df08356f36ecfac1fec09c2cf3760
dmlar <github@dmlar.com>
2025-03-06 03:53:40 -0800

    fix file structure so routing works

--- src/app/rentals/index.tsx
+++
@@ -1,9 +0,0 @@
-import rentalsData from '@/data/rentals.json';
-
-export default function RentalsIndex() {
-    return <div>{
-        rentalsData.map(rental => {
-            return <a key={rental.addressStub} href={`rentals/${rental.addressStub}/redirect`}> {rental.address.street} - {rental.title}</a>
-        })
-    }</div>
-}
